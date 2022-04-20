6a541e18f880f13c035f36bdce4a8c2413c4412c
TrevorTress <trevor.tress@outlook.com>
2022-04-20 14:19:19 -0400

    command line can generate a web page

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
+const generatePage = require('./dist/page-template');
 
+// connection to class files
 const Manager = require('./lib/Manager')
 const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
+// this roster gets employees pushed to it from the prompt functions
 const employeeRoster = []
 
+// arrays of questions to clean up prompts
 const managerQuestions = 
 [   {
         type: 'text',
@@ -72,37 +77,70 @@ const internQuestions =
     {
         type: 'input',
         name: 'school',
-        message: "What school is the intern attending?"
-    }
+        message: "What school is the intern attending?"}
 ]
 
-function initialize() {
-    promptUser()
-}
-
+// assumes user is the manager and collects their data to begin
 function promptUser() {
     inquirer.prompt(managerQuestions)
     // destructure manager data from the prompt object
     .then(({ name, id, email, office }) => {
+        // create a manager w/ that data
         const manager = new Manager(name, id, email, office)
 
+        // push manager to the employee roster and ask them about more employees
         employeeRoster.push(manager)
         console.log(employeeRoster)
         employeeAddOption()
     })    
 }
 
+// ask engineer questions
+function promptEngineer() {
+    inquirer.prompt(engineerQuestions)
+    // destructure that data
+    .then(({ name, id, email, github }) => {
+        // create an engineer with it
+        const engineer = new Engineer(name, id, email, github)
+
+        // push that engineer to the roster and restart the add function
+        employeeRoster.push(engineer)
+        console.log(employeeRoster)
+        employeeAddOption()
+    })
+}
+
+// ask intern questions
+function promptIntern() {
+    inquirer.prompt(internQuestions)
+    // destructure that data
+    .then(({ name, id, email, school }) => {
+        // create an intern with it
+        const intern = new Intern(name, id, email, school)
+
+        // push that intern to the roster and restart the add function
+        employeeRoster.push(intern)
+        console.log(employeeRoster)
+        employeeAddOption()
+    })
+}
+
+// triggered at the end of each employee creation
 function employeeAddOption() {
+    // question variables only for prompt clean-up
     let question = 
     {type: 'list', name: 'addition', message: 'Do you want to add an employee to your team?', choices: ['Yes', 'No']}
 
     let choiceQuestion =
     {type: 'list', name: 'choice', message: 'What kind of employee?', choices: ['Engineer', 'Intern']}
 
+    // ask whether user wants to add another employee
     inquirer.prompt(question)
     .then(({ addition }) => {
+        // if yes, ask them what kind
         if (addition === 'Yes') {
             inquirer.prompt(choiceQuestion)
+            // either input triggers its corresponding function
             .then(({ choice }) => {
                 if (choice == 'Intern') {
                     promptIntern()
@@ -111,41 +149,19 @@ function employeeAddOption() {
                     promptEngineer()
                 }
             })
-        } 
+        }
+        // if no, run function to apply data to html
         if (addition === 'No') {
-            console.info('awesome cool thanks')
+            const pageHTML = generatePage();
+            fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+            });
         }
     })
 }
 
-function promptEngineer() {
-    inquirer.prompt(engineerQuestions)
-    .then(({ name, id, email, github }) => {
-        const engineer = new Engineer(name, id, email, github)
-
-        employeeRoster.push(engineer)
-        console.log(employeeRoster)
-        employeeAddOption()
-    })
-}
-
-function promptIntern() {
-    inquirer.prompt(internQuestions)
-    .then(({ name, id, email, school }) => {
-        const intern = new Intern(name, id, email, school)
-
-        employeeRoster.push(intern)
-        console.log(employeeRoster)
-        employeeAddOption()
-    })
-}
-
-initialize()
-// WHEN I select the engineer option
-// THEN I am taken back to the menu
-
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+promptUser()
 
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
